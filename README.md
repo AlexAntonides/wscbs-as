947ec6fd466effdc0869b5641a5abfef4bbfc34e
Alex Antonides <Alex.M.Antonides@gmail.com>
2022-04-07 16:56:25 +0200

    The code I've written during the lecture.

---
+++ README.md
@@ -0,0 +1,42 @@
+## Getting Started
+These instructions will get you a copy up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+##### 1. Install the following software:
+- [ ] [Python](https://www.python.org/)
+
+
+### Installing
+A step by step series of examples that tell you how to get a development environment running.
+
+##### 1. Installing the modules
+After cloning the repository, navigate to the project folder and run the following command: 
+```console   
+python -m pip install flask
+python -m pip install validators
+python -m pip install pytest
+```
+
+or 
+
+```console   
+python -m pip install -r requirements.txt
+```
+
+### Deployment
+Once the modules have been installed, the project will be ready for deployment. 
+
+##### 1. Start the Application
+Run the following command to start the application
+```console   
+python main.py
+```
+
+### Testing
+The application is also suitable for testing.
+
+##### 1. Test the Application
+Run the following command to test the application.
+```console   
+pytest
+```
---
+++ test_main.py
@@ -0,0 +1,87 @@
+import json
+import pytest 
+
+from main import app as application, SHORT_URL_LEN
+
+URL = "https://www.uva.nl/"
+
+# ----- Setup -----
+@pytest.fixture
+def app():
+    yield application
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+# ----- Main -----
+def test_main_get(client):
+    LEN = 5
+    for i in range(LEN):
+        client.post("/", data=URL)
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert len(json.loads(response.get_data(as_text=True))) == LEN
+
+def test_main_post(client):
+    response = client.post("/", data=URL)
+    assert response.status_code == 201
+    assert len(response.data) == SHORT_URL_LEN
+
+def test_main_faulty_post(client):
+    response = client.post("/")
+    assert response.status_code == 400
+
+def test_main_malformed_url(client):
+    response = client.post("/", data="http://google")
+    assert response.status_code == 400
+
+def test_main_delete(client):
+    response = client.delete("/")
+    assert response.status_code == 404
+
+# ----- Handle ID -----
+def test_handle_id_get(client):
+    main_response = client.post("/", data=URL)
+    id = main_response.data.decode("utf-8")
+
+    response = client.get(f"/{id}")
+    assert response.status_code == 301
+
+def test_handle_id_missing_get(client):
+    response = client.get(f"/non-existant-id")
+    assert response.status_code == 404
+
+def test_handle_id_put(client):
+    main_response = client.post("/", data=URL)
+    id = main_response.data.decode("utf-8")
+
+    response = client.put(f"/{id}", data="https://www.google.com/")
+    assert response.status_code == 200
+
+def test_handle_id_faulty_put(client):
+    main_response = client.post("/", data=URL)
+    id = main_response.data.decode("utf-8")
+
+    response = client.put(f"/{id}")
+    assert response.status_code == 400
+
+def test_handle_id_missing_put(client):
+    response = client.put(f"/non-existant-id")
+    assert response.status_code == 404
+
+def test_handle_id_delete(client):
+    main_response = client.post("/", data=URL)
+    id = main_response.data.decode("utf-8")
+
+    response = client.delete(f"/{id}")
+    assert response.status_code == 204
+
+def test_handle_id_missing_delete(client):
+    response = client.delete(f"/non-existant-id")
+    assert response.status_code == 404
